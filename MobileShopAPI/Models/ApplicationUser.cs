c9dd17cd730dc9ac5ea5aafb8e4fbd1a7a90fc70
Anh Khoa <79366235+anhkhoa3302@users.noreply.github.com>
2023-05-23 18:39:25 +0700

    Add project files.

---
+++ MobileShopAPI/Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MobileShopAPI.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [PersonalData]
+        public string FirstName { get; set; }
+        [PersonalData]
+        public string MiddleName { get; set; }
+        [PersonalData]
+        public string LastName { get; set; }
+        [PersonalData]
+        public string Address { get; set; }
+        [PersonalData]
+        public int Status { get; set; }
+        [PersonalData]
+        [Column("createdDate")]
+        public DateTime? CreatedDate { get; set; }
+    }
+}
