ecb7185da1e546e57617b483a62367b079aa80cb
tien820 <tienlam820@gmail.com>
2023-05-27 10:48:33 +0700

    fix

--- MobileShopAPI/Program.cs
+++ MobileShopAPI/Program.cs
@@ -44,6 +44,7 @@ builder.Services.AddAuthentication(options =>
 //Inject services
 builder.Services.AddScoped<IUserService,UserService>();
 builder.Services.AddTransient<IMailService, SendGridMailService>();
+builder.Services.AddScoped<IImageService, ImageService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
