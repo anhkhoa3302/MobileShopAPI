d3d10d7e38bd50ece0660b9053f52ea34e637931
Anh Khoa <79366235+anhkhoa3302@users.noreply.github.com>
2023-05-31 13:41:12 +0700

    Migrations success

--- MobileShopAPI/Migrations/20230531063804_mig11.Designer.cs
+++ MobileShopAPI/Migrations/20230531063804_mig11.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MobileShopAPI.Data;
 
@@ -11,9 +12,10 @@ using MobileShopAPI.Data;
 namespace MobileShopAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230531063804_mig11")]
+    partial class mig11
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
