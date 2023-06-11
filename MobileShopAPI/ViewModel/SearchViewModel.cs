099df2d4fce46adf2c05496a1c6e377f27ba8854
Anh Khoa <79366235+anhkhoa3302@users.noreply.github.com>
2023-06-11 19:53:55 +0700

    search 1

---
+++ MobileShopAPI/ViewModel/SearchViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MobileShopAPI.ViewModel
+{
+    public class SearchViewModel
+    {
+        public string KeyWord { get; set; } = null!;
+        public IEnumerable<Object>? Items { get; set; }
+    }
+}
