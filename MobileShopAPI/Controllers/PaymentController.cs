c8e6d85b5560d63328a7c64b6e6d14535fb7d0d0
nnmchau01 <nguyenchau0926@gmail.com>
2023-06-30 17:37:56 +0700

    payment, statistics

--- MobileShopAPI/Controllers/PaymentController.cs
+++ MobileShopAPI/Controllers/PaymentController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MobileShopAPI.Data;
+using MobileShopAPI.Models;
+using MobileShopAPI.Responses;
 using MobileShopAPI.Services;
 using MobileShopAPI.ViewModel;
 
@@ -7,27 +11,67 @@ namespace MobileShopAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PaymentController : ControllerBase
+    public class PaymentController : Controller
     {
         private readonly IVnPayService _vnPayService;
-
-        public PaymentController(IVnPayService vnPayService)
+        private readonly UserManager<ApplicationUser> _user;
+        private readonly ApplicationDbContext _context;
+        private readonly EmailService.IEmailSender _emailSender;
+        private readonly IUserManagerService _userManagerService ;
+        public PaymentController(IVnPayService vnPayService, ApplicationDbContext context,UserManager<ApplicationUser> userManager, 
+            EmailService.IEmailSender emailSender, IUserManagerService userManagerService)
         {
             _vnPayService = vnPayService;
+            _context = context;
+            _user = userManager;
+            _emailSender = emailSender;
+            _userManagerService = userManagerService;
         }
         [HttpPost]
-        public IActionResult CreatePaymentUrl([FromBody]PaymentInformationModel model) 
+        public async Task<IActionResult>CreatePaymentUrl([FromBody]PaymentInformationModel model) 
         {
             var url = _vnPayService.CreatePaymentUrl(model, HttpContext);
             return Redirect(url);
         }
-        [HttpGet]
-        public IActionResult PaymentCallback()
+        [HttpGet("callback")]
+        public async Task<IActionResult>PaymentCallback()
         {
-            var response = _vnPayService.PaymentExecute(Request.Query);
+            var user = await _user.GetUserAsync(User);
 
-            return Redirect(response.ToString());
+            var response = _vnPayService.PaymentExecute(Request.Query);
+            
+            VnpTransaction transaction = new()
+            {
+                UserId = user.Id,
+                Id = response.TransactionId,
+                PackageId = response.packageId
+                //OrderId = response.OrderId,
+            };
+
+            //Order order = await _context.Orders.FindAsync(transaction.OrderId);
+            String message = "<p>Xin chào " +
+                            "<p><b>Chi tiết đơn hàng</b> :</p>" +
+                            //"<p><b>Địa chỉ giao</b> : " + order.Address + "</p>" +
+                            //"<p><b>Ngày thanh toán</b> : " + order.CreatedDate + "</p>" +
+                            "<p><b>Mã giao dịch</b> : " + transaction.Id + "</p>";
+            //EmailService.Message mssg = new EmailService.Message(new string[] { user.Email }, "Chi tiết đơn hàng", message);
+            //_emailSender.SendEmailAsync(mssg);
+
+            _context.Transactions.Add(transaction);
+            _context.SaveChanges();
+            return Json(true);
+
         }
+        //[HttpPost]
+        //public async Task<IActionResult> CoinPurse(string id, UpdateUserProfileViewModel model)
+        //{
+        //    var user = await _user.GetUserAsync(User);
+        //    var idpackage = _vnPayService.CoinPurse(id);
+        //    var balance = _userManagerService.UpdateUserProfileAsync(user.Id, model);
+
+
+        //}
+        
     }
     
 
---
+++ MobileShopAPI/Controllers/StatisticsController.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MobileShopAPI.Data;
+using MobileShopAPI.ViewModel;
+
+
+
+namespace MobileShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StatisticsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("monthly-registers")]
+        public async Task<IActionResult> GetMonthlyNewRegisters(int year)
+        {
+            var data = await _context.Users.Where(x => x.CreatedDate.Value.Date.Year == year)
+               .GroupBy(x => x.CreatedDate.Value.Date.Month)
+               .Select(g => new MonthlyNewUsersViewModel()
+               {
+                   Month = g.Key,
+                   NumberOfNewUser = g.Count()
+               })
+               .ToListAsync();
+
+            return Ok(data);
+        }
+        [HttpGet("FromTo-registers")]
+        public async Task<IActionResult> GetNewRegisters(string fromDate, string toDate)
+        {
+           
+            var query = from u in _context.Users
+                        select new
+                        {
+                            CreateDate = u.CreatedDate,
+                            UserId = u.Id,
+                        };
+            if (!string.IsNullOrEmpty(fromDate))
+            {
+                DateTime startDate = DateTime.ParseExact(fromDate, "dd/MM/yyyy", null);
+                query = query.Where(x => x.CreateDate >= startDate);
+            }
+            if (!string.IsNullOrEmpty(toDate))
+            {
+                DateTime endDate = DateTime.ParseExact(toDate, "dd/MM/yyyy", null);
+                query = query.Where(x => x.CreateDate < endDate);
+            }
+            var result = await query.GroupBy(x => x.CreateDate)
+                                .Select(g => new WeeklyNewUserViewModel()
+                                {
+                                    NumberOfNewUser = g.Count()
+                                })
+                                .ToListAsync();
+            return Ok(result);
+        }
+
+
+        // product
+
+        [HttpGet("monthly-product")]
+        public async Task<IActionResult> GetMonthlyNewProducts(int year)
+        {
+            var data = await _context.Products.Where(x => x.CreatedDate.Value.Date.Year == year)
+               .GroupBy(x => x.CreatedDate.Value.Date.Month)
+               .Select(g => new MonthlyNewProductViewModel() 
+               {
+                   Month = g.Key,
+                   NumberOfNewProduct = g.Count()
+               })
+               .ToListAsync();
+
+            return Ok(data);
+        }
+        [HttpGet("FromTo-Products")]
+        public async Task<IActionResult> GetNewProducts(string fromDate, string toDate)
+        {
+
+            var query = from u in _context.Products
+                        select new
+                        {
+                            CreateDate = u.CreatedDate,
+                            ProductId = u.Id,
+                        };
+            if (!string.IsNullOrEmpty(fromDate))
+            {
+                DateTime startDate = DateTime.ParseExact(fromDate, "dd/MM/yyyy", null);
+                query = query.Where(x => x.CreateDate >= startDate);
+            }
+            if (!string.IsNullOrEmpty(toDate))
+            {
+                DateTime endDate = DateTime.ParseExact(toDate, "dd/MM/yyyy", null);
+                query = query.Where(x => x.CreateDate < endDate);
+            }
+            var result = await query.GroupBy(x => x.CreateDate)
+                                .Select(g => new FromToNewProductViewModel() 
+                                {
+                                    NumberOfNewProduct = g.Count()
+                                })
+                                .ToListAsync();
+            return Ok(result);
+        }
+
+
+        //report
+
+        [HttpGet("monthly-reports")]
+        public async Task<IActionResult> GetMonthlyNewReports(int year)
+        {
+            var data = await _context.Reports.Where(x => x.CreatedDate.Value.Date.Year == year)
+               .GroupBy(x => x.CreatedDate.Value.Date.Month)
+               .Select(g => new MonthlyNewReportViewModel() 
+               {
+                   Month = g.Key,
+                   NumberOfNewReport = g.Count()
+               })
+               .ToListAsync();
+
+            return Ok(data);
+        }
+        [HttpGet("FromTo-reports")]
+        public async Task<IActionResult> GetNewReports(string fromDate, string toDate)
+        {
+
+            var query = from u in _context.Reports
+                        select new
+                        {
+                            CreateDate = u.CreatedDate,
+                            ReportId = u.Id,
+                        };
+            if (!string.IsNullOrEmpty(fromDate))
+            {
+                DateTime startDate = DateTime.ParseExact(fromDate, "dd/MM/yyyy", null);
+                query = query.Where(x => x.CreateDate >= startDate);
+            }
+            if (!string.IsNullOrEmpty(toDate))
+            {
+                DateTime endDate = DateTime.ParseExact(toDate, "dd/MM/yyyy", null);
+                query = query.Where(x => x.CreateDate < endDate);
+            }
+            var result = await query.GroupBy(x => x.CreateDate)
+                                .Select(g => new FromToNewReportViewModel() 
+                                {
+                                    NumberOfNewReport = g.Count()
+                                })
+                                .ToListAsync();
+            return Ok(result);
+        }
+
+    }
+}
